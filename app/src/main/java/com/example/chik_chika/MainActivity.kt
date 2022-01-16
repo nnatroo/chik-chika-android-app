5bd6984fbdfe79496d3ce8b0db79a61a016a9eae
nnatroo <nugonatroshvili@gmail.com>
2022-01-16 15:21:10 +0400

    6

--- app/src/main/java/com/example/chik_chika/MainActivity.kt
+++ app/src/main/java/com/example/chik_chika/MainActivity.kt
@@ -1,11 +1,20 @@
 package com.example.chik_chika
 
+
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import com.example.chik_chika.fragments.LoginFragment
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val loginFragment = LoginFragment()
+        val fm: FragmentManager = supportFragmentManager
+        fm.beginTransaction().add(R.id.main_layout, loginFragment).commit()
+
     }
 }
--- app/src/main/java/com/example/chik_chika/fragments/LoginFragment.kt
+++ app/src/main/java/com/example/chik_chika/fragments/LoginFragment.kt
@@ -1,7 +1,6 @@
 package com.example.chik_chika.fragments
 
 import android.os.Bundle
-import android.util.Patterns
 import android.view.View
 import android.widget.Button
 import android.widget.EditText
@@ -15,8 +14,6 @@ class LoginFragment : Fragment(R.layout.fragment_login){
     private lateinit var buttonLogin : Button
     private lateinit var buttonReset : Button
     private lateinit var buttonRegister : Button
-    private val emailPattern = "[a-zA-Z0-9._-]+@[a-z]+\\.+[a-z]+"
-    private val emailPattern2 = "[a-zA-Z0-9._-]+@[a-z]+\\.+[a-z]+\\.+[a-z]+"
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
@@ -45,19 +42,10 @@ class LoginFragment : Fragment(R.layout.fragment_login){
 
             if(email.isEmpty()){
                 editTextMail.error = "Mail is empty!"
-                return@setOnClickListener
             }
+
             if(password.isEmpty()){
                 editTextPassword.error = "Password is empty!"
-                return@setOnClickListener
-            }
-            if(!Patterns.EMAIL_ADDRESS.matcher(email).matches()){
-                editTextMail.error = "Enter valid e-mail address. Try again."
-                return@setOnClickListener
-            }
-            if (!(email.matches(emailPattern.toRegex())) && !(email.matches(emailPattern2.toRegex()))){
-                editTextMail.error = "Incorrect Email"
-                return@setOnClickListener
             }
 
 
