7fa7dd48105d86963b2af06e5f949d378e29ad32
nnatroo <nugonatroshvili@gmail.com>
2022-01-17 23:23:07 +0400

    timeline

---
+++ app/src/main/java/com/example/chik_chika/fragmentsTimeline/Profile.kt
@@ -0,0 +1,7 @@
+package com.example.chik_chika.fragmentsTimeline
+
+import androidx.fragment.app.Fragment
+import com.example.chik_chika.R
+
+class Profile : Fragment(R.layout.fragment_profile) {
+}
