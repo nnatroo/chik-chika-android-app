65117bf515c2d0f7b75f2bf644bb63cb43d13394
nnatroo <nugonatroshvili@gmail.com>
2022-01-18 02:57:55 +0400

    yvelaferi

--- app/src/main/java/com/example/chik_chika/TimelineActivity.kt
+++ app/src/main/java/com/example/chik_chika/TimelineActivity.kt
@@ -32,4 +32,6 @@ class TimelineActivity : AppCompatActivity() {
         navView.setupWithNavController(controller)
 
     }
+
+
 }
--- app/src/main/java/com/example/chik_chika/fragmentsTimeline/ProfileFragment.kt
+++ app/src/main/java/com/example/chik_chika/fragmentsTimeline/ProfileFragment.kt
@@ -1,7 +1,110 @@
 package com.example.chik_chika.fragmentsTimeline
 
+import android.app.Activity.RESULT_OK
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.graphics.BitmapFactory
+import android.os.Build
+import android.os.Bundle
+import android.provider.MediaStore
+import android.view.View
+import android.widget.ImageView
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
 import androidx.fragment.app.Fragment
 import com.example.chik_chika.R
 
+
+
 class ProfileFragment : Fragment(R.layout.fragment_profile) {
+
+    private lateinit var imageVewPicture : ImageView
+
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        init()
+
+        imageVewPicture.setOnClickListener( View.OnClickListener {
+            checkPermission()
+            loadImage()
+        })
+
+    }
+
+    private fun init(){
+        imageVewPicture = requireView().findViewById(R.id.imageViewPicture)
+
+    }
+
+
+    val READIMAGE:Int=253
+    fun checkPermission(){
+
+        if(Build.VERSION.SDK_INT>=23){
+            if(activity?.let {
+                    ActivityCompat.checkSelfPermission(
+                        it,
+                        android.Manifest.permission.READ_EXTERNAL_STORAGE)
+                } !=
+                PackageManager.PERMISSION_GRANTED){
+
+                requestPermissions(arrayOf( android.Manifest.permission.READ_EXTERNAL_STORAGE),READIMAGE)
+                return
+            }
+        }
+
+    }
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+
+        when(requestCode){
+            READIMAGE->{
+                if(grantResults[0]==PackageManager.PERMISSION_GRANTED){
+                    loadImage()
+                }else{
+                    Toast.makeText(activity,"Cannot access your images",Toast.LENGTH_LONG).show()
+                }
+            }
+            else-> super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        }
+
+
+    }
+
+    val PICK_IMAGE_CODE=123
+    fun loadImage(){
+
+        var intent=Intent(Intent.ACTION_PICK,
+            android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+        startActivityForResult(intent,PICK_IMAGE_CODE)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if(requestCode==PICK_IMAGE_CODE  && data!=null && resultCode == RESULT_OK){
+
+            val selectedImage=data.data
+            val filePathColum= arrayOf(MediaStore.Images.Media.DATA)
+            val cursor= activity?.contentResolver?.query(selectedImage!!,filePathColum,null,null,null)
+            cursor!!.moveToFirst()
+            val coulomIndex=cursor!!.getColumnIndex(filePathColum[0])
+            val picturePath=cursor!!.getString(coulomIndex)
+            cursor!!.close()
+            imageVewPicture.setImageBitmap(BitmapFactory.decodeFile(picturePath))
+        }
+
+    }
+
+
+
+
+
+
+
+
+
+
+
 }
